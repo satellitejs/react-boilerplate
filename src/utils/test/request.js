d19728371a2c2a300c429da6d66c049de4abc29f
吉武篤司 <0nanopx@gmail.com>
2016-05-17 14:35:03 +0900

    updated test for request util

--- src/utils/test/request.js
+++ src/utils/test/request.js
@@ -16,18 +16,23 @@ const failResponse = {
 mock.onGet('/test/success').reply(200, successResponse);
 mock.onGet('/test/fail').reply(500, failResponse);
 
-test('Util: request', (t) => {
-  request('get', '/test/success', {}).then((res) => {
-    t.deepEqual(res.data, successResponse, 'should be able to send a request successfully');
-    return res;
+test('Util: request', (_t) => {
+
+  _t.test('request success', (t) => {
+    return request('get', '/test/success', {}).then((res) => {
+      t.deepEqual(res.data, successResponse, 'should be able to send a request successfully');
+      return res;
+    });
   });
 
-  return request('get', '/test/fail', {}).then(() => {
-    throw new Error('should have failed');
-  }, (err) => {
-    if (!(err instanceof Error)) {
-      throw new Error('should have thrown an instance of ' + Error);
-    }
-    t.pass('should throw an error if request failed');
+  _t.test('request fail', (t) => {
+    return request('get', '/test/fail', {}).then(() => {
+      throw new Error('should have failed');
+    }, (err) => {
+      if (!(err instanceof Error)) {
+        throw new Error('should have thrown an instance of ' + Error);
+      }
+      t.pass('should throw an error if request failed');
+    });
   });
 });
